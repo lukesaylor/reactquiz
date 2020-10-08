21dd57711a3fd389942e1f616ce784a1f84472ff
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-08 14:09:11 -0400

    Updates

---
+++ precache-manifest.809d7f5389acbf828564c1db22aefc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1702b4bd0f08e3c0d7fc7fdbe163431c",
+    "url": "/reactquiz/index.html"
+  },
+  {
+    "revision": "fb62415980a88b469eac",
+    "url": "/reactquiz/static/js/2.d8aabf8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactquiz/static/js/2.d8aabf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b314b4347c4fc615ba",
+    "url": "/reactquiz/static/js/main.0947203c.chunk.js"
+  },
+  {
+    "revision": "42775025bb703499c181",
+    "url": "/reactquiz/static/js/runtime-main.c788a892.js"
+  },
+  {
+    "revision": "272d6bd854ee023787b9a801aa89ba0e",
+    "url": "/reactquiz/static/media/bgimage.272d6bd8.jpg"
+  }
+]);
